cbf49287c965d48e14f7fd635ef41509319ed9c1
wedojava <wedojava@gmail.com>
2020-07-28 19:42:39 +0800

    create root folder if it is not exist.

--- main.go
+++ main.go
@@ -5,20 +5,29 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"path/filepath"
 
 	"github.com/wedojava/goftp/ftp"
 )
 
 var (
-	port    int
-	rootDir string
+	port int
+	root string
 )
 
 func init() {
 	flag.IntVar(&port, "port", 8000, "port number")
-	flag.StringVar(&rootDir, "rootDir", "public", "root Directory")
+	flag.StringVar(&root, "root", "public", "root Directory")
 	flag.Parse()
+	// Create root dir if it is not exist.
+	_, err := os.Stat(root)
+	if err != nil && os.IsNotExist(err) {
+		err = os.MkdirAll(root, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func main() {
@@ -39,7 +48,7 @@ func main() {
 
 func handleConn(c net.Conn) {
 	defer c.Close()
-	absPath, err := filepath.Abs(rootDir)
+	absPath, err := filepath.Abs(root)
 	if err != nil {
 		log.Fatal(err)
 	}
